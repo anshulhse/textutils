b3b74ac1a01827267fa2b63c7b8fbb5098571991
Anshul Gupta <91626299+anshulhse@users.noreply.github.com>
2022-07-05 12:31:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
   return (
     
     <>
-    <Navbar title="Utilities" mode={mode} toggleMode={toggleMode} />
+    <Navbar title="TextUtilities" mode={mode} toggleMode={toggleMode} />
     <Alert alert={alert} />
     <TextForm  path="/" showAlert={showAlert} mode={mode} heading="Enter the heading" />
     </>
